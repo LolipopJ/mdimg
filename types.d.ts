4e55f71fbb050c57c367b2582279e51fac5135c4
LolipopJ <lolipop0703@qq.com>
2023-04-03 15:54:00 +0800

    chore: update type defines

--- types.d.ts
+++ types.d.ts
@@ -1,31 +1,24 @@
-// import puppeteer from "puppeteer";
+import type { LaunchOptions } from "puppeteer";
 
-interface IProps {
-  mdText: string;
-  mdFile: string;
+export interface IOptions {
+  mdText?: string;
+  mdFile?: string;
   outputFilename?: string;
   type?: "jpeg" | "png" | "webp";
   width?: number;
   height?: number;
   encoding?: "binary" | "base64";
   quality?: number;
-  htmlTemplate?: "";
-  cssTemplate?: "";
+  htmlTemplate?: string;
+  cssTemplate?: string;
   log?: boolean;
-  // puppeteerProps: puppeteer
+  puppeteerProps?: LaunchOptions;
 }
 
-export async function convert2img({
-  mdText,
-  mdFile,
-  outputFilename,
-  type = "png",
-  width = 800,
-  height = 600,
-  encoding = "binary",
-  quality,
-  htmlTemplate = "default",
-  cssTemplate = "default",
-  log = false,
-  puppeteerProps,
-}: IProps): string;
+export interface IResponse {
+  data: string | Buffer;
+  path?: string;
+  html: string;
+}
+
+export type convert2img = (props: IOptions) => Promise<IResponse>;
