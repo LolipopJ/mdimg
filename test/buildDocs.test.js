fe8626de17324246d30040d80c397d2aca1e690d
LolipopJ <42314340+LolipopJ@users.noreply.github.com>
2024-07-22 16:09:59 +0800

    test: add test cases

--- test/buildDocs.test.js
+++ test/buildDocs.test.js
@@ -1,69 +1,96 @@
-const { convert2img } = require("../lib/mdimg.js");
+const { mdimg } = require("../lib/mdimg.js");
 const { resolve } = require("path");
-const { readFileSync } = require("fs");
+const { existsSync, rmSync, readFileSync } = require("fs");
+const { execSync } = require("child_process");
 
-const testFilename = resolve(__dirname, "test.md");
-const outputFilenameBase = resolve(__dirname, "../docs");
+const inputFilenameTest = resolve(__dirname, "test.md");
+const inputFilenameTestTemplateWords = resolve(
+  __dirname,
+  "testTemplateWords.md",
+);
+const outputDir = resolve(__dirname, "../docs");
 
-test("Convert to image with Empty CSS template", async () => {
-  const outputFilename = resolve(`${outputFilenameBase}/empty.png`);
-  const convertRes = await convert2img({
-    inputFilename: testFilename,
+if (existsSync(outputDir)) {
+  rmSync(outputDir, { recursive: true });
+}
+
+test("Convert to image with default template, using `inputText`, `htmlText` and `cssText`", async () => {
+  const inputTextTest = readFileSync(inputFilenameTest);
+
+  const htmlTemplateDefaultFilename = resolve(
+    __dirname,
+    "../template/html/default.html",
+  );
+  const htmlTemplateDefaultText = readFileSync(htmlTemplateDefaultFilename);
+
+  const cssTemplateDefaultFilename = resolve(
+    __dirname,
+    "../template/css/default.css",
+  );
+  const cssTemplateDefaultText = readFileSync(cssTemplateDefaultFilename);
+
+  const outputFilename = resolve(`${outputDir}/default.png`);
+
+  const convertRes = await mdimg({
+    inputText: inputTextTest,
     outputFilename,
-    cssTemplate: "empty",
+    htmlText: htmlTemplateDefaultText,
+    cssText: cssTemplateDefaultText,
     log: true,
   });
+
   expect(convertRes.path).toBe(outputFilename);
 });
 
-test("Convert to image with Default template", async () => {
-  const outputFilename = resolve(`${outputFilenameBase}/default.png`);
-  const convertRes = await convert2img({
-    inputFilename: testFilename,
-    outputFilename,
-    log: true,
-  });
-  expect(convertRes.path).toBe(outputFilename);
+test("Convert to image with `empty` CSS template, using command", async () => {
+  const outputFilename = resolve(`${outputDir}/empty.png`);
+
+  expect(() =>
+    execSync(
+      `node bin/mdimg -i ${inputFilenameTest} -o ${outputFilename} --css empty`,
+    ),
+  ).not.toThrow();
 });
 
-test("Convert to image with Github CSS template", async () => {
-  const outputFilename = resolve(`${outputFilenameBase}/github.png`);
-  const convertRes = await convert2img({
-    inputFilename: testFilename,
+test("Convert to image with `github` CSS template", async () => {
+  const outputFilename = resolve(`${outputDir}/github.png`);
+
+  const convertRes = await mdimg({
+    inputFilename: inputFilenameTest,
     outputFilename,
     cssTemplate: "github",
     width: 1000,
     log: true,
   });
+
   expect(convertRes.path).toBe(outputFilename);
 });
 
-test("Convert to image with Github Dark CSS template, using `inputText`", async () => {
-  const testFileText = readFileSync(testFilename).toString();
-  const outputFilename = resolve(`${outputFilenameBase}/githubDark.png`);
-  const convertRes = await convert2img({
-    inputText: testFileText,
+test("Convert to image with `githubDark` CSS template", async () => {
+  const outputFilename = resolve(`${outputDir}/githubDark.png`);
+
+  const convertRes = await mdimg({
+    inputFilename: inputFilenameTest,
     outputFilename,
     cssTemplate: "githubDark",
     width: 1000,
     log: true,
   });
+
   expect(convertRes.path).toBe(outputFilename);
 });
 
-test("Convert to image with Words template, using `inputText`", async () => {
-  const testFilenameTemplateWords = resolve(__dirname, "testTemplateWords.md");
-  const testFileTemplateWordsText = readFileSync(
-    testFilenameTemplateWords,
-  ).toString();
-  const outputFilename = resolve(`${outputFilenameBase}/words.png`);
-  const convertRes = await convert2img({
-    inputText: testFileTemplateWordsText,
+test("Convert to image with `words` template", async () => {
+  const outputFilename = resolve(`${outputDir}/words.png`);
+
+  const convertRes = await mdimg({
+    inputFilename: inputFilenameTestTemplateWords,
     outputFilename,
     htmlTemplate: "words",
     cssTemplate: "words",
     width: 1200,
     log: true,
   });
+
   expect(convertRes.path).toBe(outputFilename);
 });
